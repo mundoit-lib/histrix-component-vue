9d435cdddeda07edb4a008f000f4b05c108f1a8b
Pablo Bagliere <pablo@mundoit.com.ar>
2026-01-22 14:29:03 -0300

    feat: eliminar archivos obsoletos y actualizar configuración de publicación para el paquete de componentes Vue

--- app-extension/src/boot/register.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue';
-import VuePlugin from '../../../quasar-ui-histrix-client/src/index.js';
-// import histrixApi from '../../../quasar-ui-histrix-client/src/services/histrixApi.js';
-
-Vue.use(VuePlugin);
-// Vue.prototype.$histrixApi = histrixApi;
